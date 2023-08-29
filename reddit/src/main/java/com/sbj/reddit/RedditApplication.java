f2f9d2f4f5c0bfd8128fb2b90a4c2b54c3e63850
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-08-30 02:33:24 +0530

    Initialized Spring Boot app and added new dependencies
    Initialized Spring Boot 2 application using Spring Initializer for Java 17 with Kotlin DSL gradle, added few dependencies explicitly such as JWT for token-based authentication and record time span, documented the build.gradle.kts and configured Java 17 as the target level Java API

---
+++ reddit/src/main/java/com/sbj/reddit/RedditApplication.java
@@ -0,0 +1,13 @@
+package com.sbj.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditApplication.class, args);
+	}
+
+}
